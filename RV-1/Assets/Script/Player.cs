a6416d32f0e217c6ccd7347616b781a5e337ad37
GabrielFSilva <gabrielsilva.prog@gmail.com>
2018-08-29 14:56:16 -0300

    Project Base

---
+++ RV-1/Assets/Script/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float movementSpeed = 5f;
+    public float rotationSpeed = 120f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move(Input.GetAxis("Vertical"));
+        //Debug.Log(Input.GetAxis("Mouse X"));
+        RotateBody(Input.GetAxis("Mouse X"));
+
+    }
+
+    private void Move(float multiplier)
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * multiplier * movementSpeed);
+    }
+
+    private void RotateBody(float multiplier)
+    {
+        transform.Rotate(Vector3.up * multiplier * rotationSpeed);
+    }
+}
