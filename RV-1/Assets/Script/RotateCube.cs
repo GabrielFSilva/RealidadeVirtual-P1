8b494ead4f2f5ad80030a22e351fb575a475ffaf
GabrielFSilva <gabrielsilva.prog@gmail.com>
2018-09-01 18:45:42 -0300

    Google VR Setup

---
+++ RV-1/Assets/Script/RotateCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour {
+
+    public float rotationSpeed = 120f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+	}
+
+    public void ChangeRotationSpeed()
+    {
+        rotationSpeed *= -1f;
+    }
+}
