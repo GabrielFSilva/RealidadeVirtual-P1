272a19e8ffa72b7d164c6403445e67b08518e624
GabrielFSilva <gabrielsilva.prog@gmail.com>
2018-09-04 13:20:03 -0300

    Arrows/Markers Setup

--- RV-1/Assets/Script/Player.cs
+++ RV-1/Assets/Script/Player.cs
@@ -8,60 +8,43 @@ public class Player : MonoBehaviour {
     public float rotationSpeed = 60f;
 
     public Camera playerCamera;
-    public float cameraRotationSpeed = 5f;
-    [Range(-1f, 0f)]
-    public float cameraMinPitch = -0.4f;
-    [Range(0f, 1f)]
-    public float cameraMaxPitch = 0.4f;
-    [Range(-1f, 0f)]
-    public float cameraMinYaw = -0.4f;
-    [Range(0f, 1f)]
-    public float cameraMaxYaw = 0.4f;
-    // Use this for initialization
+
+    private int selectedSideMark = 0;
+    private int selectedFrontMark = 0;
+
     void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-#if UNITY_EDITOR
+	void LateUpdate () {
         Move(Input.GetAxis("Vertical"));
-#endif
-        RotateCamera(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * selectedSideMark, Space.World);
+        transform.Translate(Vector3.forward * Time.deltaTime * movementSpeed * selectedFrontMark);
     }
 
     private void Move(float value)
     {
         transform.Translate(Vector3.forward * Time.deltaTime * value * movementSpeed);
     }
-
-    private void RotateBody(float value)
+    public void FrontMarkSelected(int mark)
     {
-        transform.Rotate(Vector3.up * Time.deltaTime * value * rotationSpeed);
+        selectedFrontMark = mark;
     }
 
-    private void RotateCamera(float xValue, float yValue)
+    public void FrontMarkDeselected()
     {
-        playerCamera.transform.Rotate(Vector3.right * yValue * -cameraRotationSpeed);
-        playerCamera.transform.Rotate(Vector3.up * xValue * cameraRotationSpeed);
-        Quaternion cameraQuaternion = playerCamera.transform.localRotation;
-        cameraQuaternion.x = Mathf.Clamp(cameraQuaternion.x, cameraMinPitch, cameraMaxPitch);
-        cameraQuaternion.y = Mathf.Clamp(cameraQuaternion.y, cameraMinYaw, cameraMaxYaw);
+        selectedFrontMark = 0;
+    }
 
-        
 
-        Vector3 cameraEuler = cameraQuaternion.eulerAngles;
-        cameraEuler.z = 0f;
+    public void SideMarkSelected(int mark)
+    {
+        selectedSideMark = mark;
+    }
 
-        playerCamera.transform.localEulerAngles = cameraEuler;
-        if (Mathf.Abs(cameraQuaternion.y) >= 0.65f * cameraMaxYaw)
-        {
-            RotateBody(cameraQuaternion.y * cameraQuaternion.w);
-        }
-        if (Mathf.Abs(cameraQuaternion.x) >= 0.5f * cameraMaxPitch)
-        {
-            Move(cameraQuaternion.x * -cameraQuaternion.w);
-        }
-
+    public void SideMarkDeselected()
+    {
+        selectedSideMark = 0;
     }
 }
